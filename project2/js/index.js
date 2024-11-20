71e73234fc19178054cb2f25a77ab5a6720d8265
zzwhoops-home <zd@5rock.org>
2024-11-20 10:51:04 -0500

    add basic dropdown styling

--- project2/js/index.js
+++ project2/js/index.js
@@ -1,3 +1,14 @@
 window.onload = (e) => {
-
+    loadContents();
 }
+
+const loadContents = () => {
+    // get dropdown button
+    const dropdown = document.querySelector(".dropbtn");
+
+    // add event listeners
+    dropdown.addEventListener("click", dropdown);
+};
+
+const dropdown = (e) => {
+};
