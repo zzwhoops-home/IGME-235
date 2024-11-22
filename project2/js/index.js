1c8a135794981c6808abafe1470eba653277f5c4
zzwhoops-home <zd@5rock.org>
2024-11-21 20:25:52 -0500

    append images to page

--- project2/js/index.js
+++ project2/js/index.js
@@ -42,41 +42,53 @@ const getSearch = async (e) => {
 
     const data = await getImagesBySearch(searchTerm);
 
-    const element = document.createElement("p");
     createResultDiv(data);
-
-    resultPanel.appendChild(element);
 }
 
 const createResultDiv = (dataArr) => {
-    dataArr.array.forEach(data => {
-        
+    dataArr.forEach(data => {
+        // create container div
+        const resultDiv = document.createElement('div');
+        resultDiv.classList.add('result');
+        // set "alt" text for background
+        resultDiv.title = data.alt_text;
+    
+        // set background image
+        resultDiv.style.setProperty('--result-bg-img', `url('${data.image_URL}')`);
+    
+        // create star icon
+        const starIcon = document.createElement('i');
+        starIcon.classList.add('fa-regular', 'fa-star');
+    
+        // create link element with title
+        const link = document.createElement('a');
+        link.href = data.image_URL;
+        link.innerHTML = data.title;
+    
+        // append to container
+        resultDiv.appendChild(starIcon);
+        resultDiv.appendChild(link);
+    
+        // append to container
+        const container = document.querySelectorAll('.result-block');
+        const lastContainer = container[container.length - 1];
+
+        // if the container is full, create a new result block
+        if (!lastContainer || lastContainer.childElementCount >= 3) {
+            // create new result block
+            const resultBlockDiv = document.createElement('div');
+            resultBlockDiv.classList.add('result-block');
+
+            // add result block 
+            const results = document.querySelector("#results");
+            results.appendChild(resultBlockDiv);
+
+            // append first result div
+            resultBlockDiv.appendChild(resultDiv);
+        }
+        else {
+            // otherwise, just append
+            lastContainer.appendChild(resultDiv);
+        }
     });
-    let data = dataArr[0];
-
-    // create container div
-    const resultDiv = document.createElement('div');
-    resultDiv.classList.add('result');
-    // set "alt" text for background
-    resultDiv.title = data.alt_text;
-
-    // set background image
-    resultDiv.style.setProperty('--result-bg-img', `url('${data.image_URL}')`);
-
-    // create star icon
-    const starIcon = document.createElement('i');
-    starIcon.classList.add('fa-regular', 'fa-star');
-
-    // create link element with title
-    const link = document.createElement('a');
-    link.href = data.image_URL;
-    link.innerHTML = data.title;
-
-    // append to container
-    resultDiv.appendChild(starIcon);
-    resultDiv.appendChild(link);
-
-    // append to container
-    const container = document.querySelector('.result-block');
-    container.appendChild(resultDiv);
 }
