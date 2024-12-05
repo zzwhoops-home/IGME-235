45dcebd4cd13ee956f79362cb303feba46316cb2
zzwhoops-home <zd@5rock.org>
2024-12-05 12:08:58 -0500

    add Game class

--- project3/js/modules/Game.js
+++ project3/js/modules/Game.js
@@ -1,10 +1,12 @@
 export class Game {
-    
 
     constructor(level, score, moves, timer) {
         this.level = level;
         this.score = score;
         this.moves = moves;
-        this.timer = timer;
+        this.levelTime = timer;
+
+        // this should be used for decrementing 
+        this.curTimer = timer;
     }
 }
