4d4ff23c650d0a8a411689d1340cc195d4f5f532
zzwhoops-home <zd@5rock.org>
2024-12-06 00:38:53 -0500

    get buttons

--- project3/js/index.js
+++ project3/js/index.js
@@ -16,10 +16,20 @@ window.onload = (e) => {
  * Loads the contents of the page on page load
  */
 const loadContents = async () => {
+    // get move buttons
+    const swap = document.querySelector('#swap');
+    const scale = document.querySelector('#scale');
+    const pivot = document.querySelector('#pivot');
+
+    // get powerup buttons
+    const powerups = document.querySelectorAll(".powerup");
+
+    // stats paragraphs
     const levelP = document.querySelector("#stats #level");
     const scoreP = document.querySelector("#stats #score");
     const movesP = document.querySelector("#stats #moves");
     const timerP = document.querySelector("#stats #timer");
 
+    // create class instances
     game = new Game(levelP, scoreP, movesP, timerP);
 };
